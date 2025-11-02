fa2ee05d58c6dcc7ab7c31f8bec94c518bd14e55
Sarang.Kher <Sarang.Kher@ibm.com>
2025-11-02 13:06:15 +0530

    Add TLS and random providers to provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "libvirt" {
   uri = "qemu:///system"
 }
+provider "tls" {
+  # Configuration options can be added here if needed
+}
+provider "random" {
+  # Configuration options can be added here if needed
+}     
